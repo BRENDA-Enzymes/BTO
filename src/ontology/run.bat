e5e1af1e46e6cbed465a8c348510c3975467ca6f
BRENDA-Enzymes <github@brenda-enzymes.org>
2020-04-22 21:56:49 +0200

    release 2020.2

--- src/ontology/run.bat
+++ src/ontology/run.bat
@@ -1,1 +1,6 @@
-docker run -v %cd%\..\..\:/work -w /work/src/ontology --rm -ti obolibrary/odkfull %*
+@echo off
+set "_path=%%cd%%"
+for %%a in ("%_path%") do set "p_dir=%%~dpa"
+for %%a in (%p_dir:~0,-1%) do set "p2_dir=%%~dpa"
+for %%a in (%p2_dir:~0,-1%) do set "p3_dir=%%~dpa"
+docker run -v %p3_dir%:/work -w /work/src/ontology --rm -ti obolibrary/odkfull %*
